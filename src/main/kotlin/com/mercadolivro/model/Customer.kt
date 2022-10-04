a61c54b762410f2c5c3cdd6363eb464dd4d339f8
Dev-JeanSantos <jeancbsan@gmail.com>
2022-10-03 21:00:19 -0300

    h2 database implementation and jpa-hibernator

--- src/main/kotlin/com/mercadolivro/model/Customer.kt
+++ src/main/kotlin/com/mercadolivro/model/Customer.kt
@@ -1,7 +1,10 @@
 package com.mercadolivro.model
 
+import javax.persistence.*
 
+@Entity
 data class Customer (
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private val id: Long? =  null,
     private val name: String,
     private val email: String
